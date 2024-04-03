9ee8dd6702c1dc074b61d9d585c8834fa32ab99a
kaf.ahmadi <kaf.ahmadi@tapsi.cab>
2024-04-03 11:25:46 +0330

    Add poetry for env managment

--- back/lifegoals/flows/models.py
+++ back/lifegoals/flows/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -6,6 +7,7 @@ from django.db import models
 
 class Goal(models.Model):
     GOAL_TYPES = [
+        
         ("I", "Important"),
         ("N", "Not Important"),
     ]
@@ -20,3 +22,4 @@ class Goal(models.Model):
 class Flow(models.Model):
     name = models.CharField(max_length=256)
     goals = models.ManyToManyField(Goal)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
